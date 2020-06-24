254724f6a67e194555207e6213669bfc2e69cb7e
ManavRao <rao.manav20@gmail.com>
2020-06-24 18:04:53 +0530

    updated image issue

--- sonnet/routes.py
+++ sonnet/routes.py
@@ -58,6 +58,16 @@ def logout():
     logout_user()
     return redirect(url_for('search'))
 
+def crop_center(pil_img, crop_width, crop_height):
+    img_width, img_height = pil_img.size
+    return pil_img.crop(((img_width - crop_width) // 2,
+                         (img_height - crop_height) // 2,
+                         (img_width + crop_width) // 2,
+                         (img_height + crop_height) // 2))
+
+def crop_max_square(pil_img):
+    return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
+
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
@@ -65,6 +75,7 @@ def save_picture(form_picture):
     picture_path = os.path.join(app.root_path, 'static/profile_pictures', picture_fn)
     output_size = (150, 150)
     i = Image.open(form_picture)
+    i = crop_max_square(i).resize(output_size, Image.LANCZOS)
     i.thumbnail(output_size)
     i.save(picture_path)
     return picture_fn
